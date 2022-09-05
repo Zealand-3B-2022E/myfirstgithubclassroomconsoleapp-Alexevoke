0fb252da64c3a5c69c9c5decc993e029d9a2a0d8
Alexander Gerdil <alex650w@edu.zealand.dk>
2022-09-05 12:45:00 +0200

    added Demo class

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class Demo
+    {
+
+        public Demo(string navn, string telefon, int alder)
+        {
+            Navn = navn;
+            Telefon = telefon;
+            Alder = alder;
+        }
+
+        public string Navn { get; set; }
+
+        public string Telefon { get; set; }
+
+        public int Alder { get; set; } 
+    }
+}
