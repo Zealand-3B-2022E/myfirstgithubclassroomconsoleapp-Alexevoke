e1e1bfd9b3996db7e0cc1d0492fed987805bd977
Alexander Gerdil <alex650w@edu.zealand.dk>
2022-09-05 13:02:51 +0200

    Added default constructor og oprettet to objekter og printet deres data ud

--- MyFirstGitHubClassRoomConsoleApp60/Demo.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -9,6 +9,13 @@ namespace MyFirstGitHubClassRoomConsoleApp60
     public class Demo
     {
 
+        public Demo()
+        {
+            Navn = "Dummy";
+            Telefon = "22004466";
+            Alder = 19;
+        }
+
         public Demo(string navn, string telefon, int alder)
         {
             Navn = navn;
--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// See https://aka.ms/new-console-template for more information
+using MyFirstGitHubClassRoomConsoleApp60;
+
+Demo demo = new Demo();
+Demo demo2 = new Demo("Thomas", "11223344", 21);
+
+Console.WriteLine(demo.Navn);
+Console.WriteLine(demo.Telefon);
+Console.WriteLine(demo.Alder);
+
+Console.WriteLine();
+
+Console.WriteLine(demo2.Navn);
+Console.WriteLine(demo2.Telefon);
+Console.WriteLine(demo2.Alder);
+
+Console.WriteLine();
+Console.WriteLine("Press any key to close the program...");
+Console.ReadKey();
